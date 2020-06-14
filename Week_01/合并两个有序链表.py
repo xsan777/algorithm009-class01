aa93d4f1c115306db348dac6d8bca08c21ac4598
sxan777 <1315969544@qq.com>
2020-06-14 21:48:00 +0800

    第四周

--- Week_01/合并两个有序链表.py
+++ Week_01/合并两个有序链表.py
@@ -21,7 +21,7 @@ class Solution:
         if l1 and l2:
             if l1.val > l2.val: l1, l2 = l2, l1
             l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1 or l2
+        return l1 or l2
 
     def mergeTwoLists_2(self, l1, l2):
         """递归解法"""
